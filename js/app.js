4ca59c9db29df7b4c8c39edb1fd3badb42728f60
Simon-OS <38255916+Simon-OS@users.noreply.github.com>
2020-02-18 23:15:16 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -73,13 +73,47 @@ $(document).ready(function() {
           }
       ]
   });
-
 });
+
+
+
+// header dropdown mobile
+
+let dropdown = document.querySelector(".header-menu__list-item.dropdown");
+let headerBtn = document.querySelector(".header-menu__btn-toggle");
+let headerMenu = document.querySelector(".header-menu");
+
+function headerMenuToggle() {
+
+  if (headerMenu.classList.contains("active")) {
+
+    headerMenu.classList.remove("active");
+    headerBtn.innerHTML = `<i class="fa fa-bars"></i>`;
+
+  } else {
+
+    headerMenu.classList.add("active");
+    headerBtn.innerHTML = `<i class="fa fa-times"></i>`;
+
+  }
+}
+
+headerBtn.addEventListener("click", headerMenuToggle);
+
+dropdown.addEventListener("click", () => {
+  dropdown.classList.toggle("active")
+  headerMenu.classList.toggle("active-dropdown");
+});
+
+
+
+// portfolio modal
 
 let portfolioItems = document.querySelectorAll(".portfolio__item");
 let portfolioModalBody = document.querySelector("#portfolioModal .modal-body");
 
 function appendImgToModal(ev) {
+  
     let image = ev.target.cloneNode();
 
     portfolioModalBody.innerHTML = "";
